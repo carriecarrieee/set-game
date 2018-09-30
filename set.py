0398d37adde349ac8cf875a6ea505465608e6775
Carrie Li <carrie.t.li@gmail.com>
2018-09-30 07:19:55 +0000

    Moved class attributes outside of constructor, fixed SetCard repr(), added SetGame repr(), modified is_set() to take in instances as parameters (instead of lists), completed find_set(), added test printouts

--- set.py
+++ set.py
@@ -8,38 +8,49 @@ from random import shuffle
 class SetCard:
     """Models a Set card, and each card has a shape, shading, and count."""
 
-    def __init__(self, shape, shading, count):
+    # Class attributes
+    shapes = ["oval", "squiggle", "diamond"]
+    shadings = ["solid", "striped", "open"]
+    counts = [1, 2, 3]
+
+
+    def __init__(self, shape="oval", shading="solid", count=1): # default card
+        
         self.shape = shape
         self.shading = shading
         self.count = count
 
 
     def __repr__(self):
-        """Prints representation of a SetCard:
+        """Prints representation of a SetCard"""
 
-        >>> newCard = SetCard('oval', 'striped', 3)
-        >>> newCard.__repr__()
-        ['oval', 'striped', 3]
-
-        """
-
-        print [self.shape, self.shading, self.count]
+        return "{}, {}, {}".format(self.shape, self.shading, self.count)
 
         
-class SetGame(SetCard):
+class SetGame:
     """Generates deck of 27 Set cards that is stored within the class instance
        on initialization.
     """
     
     def __init__(self):
-
-        shapes = ["oval", "squiggle", "diamond"]
-        shadings = ["solid", "striped", "open"]
-        counts = [1, 2, 3]
         
-        self.deck = [SetCard(shape, shading, count) for shape in shapes \
-                                                    for shading in shadings \
-                                                    for count in counts]
+        self.deck = [SetCard(shape, shading, count) \
+            for shape in SetCard.shapes \
+            for shading in SetCard.shadings \
+            for count in SetCard.counts]
+
+        self.table = [] # List that tracks cards currently on table
+
+
+    def __repr__(self):
+        """Prints string representation of deck."""
+
+        deck_lst = []
+
+        for card in self.deck:
+            deck_lst.append(str(card))
+
+        return "\n".join(deck_lst)
 
 
     def deal(self, num_cards):
@@ -47,87 +58,85 @@ class SetGame(SetCard):
            The instance variable "table" tracks the cards that are currently on
            the table. This function deals num_cards random cards onto the table 
            from the deck.
-
-           myGame = SetGame()
-           myGame.deal(4)
         """
 
         self.num_cards = num_cards # Number of cards to be dealt
-        self.table = [] # List that tracks cards currently on table
 
         shuffle(self.deck) # Shuffle cards before dealing
 
         for i in range(num_cards):
             if not self.deck: # In case the deck is empty
-                print "Deck is empty!"
-                break
+                raise Exception("Deck is empty! Please create new deck.")
 
-            # Pops num_cards off of the deck and adds it to the table list
+            # Pops num_cards off of the deck and adds it to table
             self.table.append(self.deck.pop())
 
-        return self.table
-# myGame = SetGame()
-# print myGame.deal(4)        
+        return self.table # list of SetCard instances
 
-# How do I print self.table without overriding the __repr__ method in superclass?
 
     def is_set(self, card1, card2, card3):
-        """Rules of the game Set: 
-           https://paper.dropbox.com/doc/lYHYZDoLQtlINtyyEYSVi
-    
-           Accepts three cards and returns a boolean indicating whether the 
+        """Accepts three cards and returns a boolean indicating whether the 
            three cards make up a valid set. A set is valid if all three
            conditions hold:
             1. Shape: all the same shape OR all different shapes
             2. Shading: all the same shading OR all different shading
             3. Count: all the same count OR all different count
-
-            >>> myGame = SetGame()
-            >>> myGame.is_set(["oval", "solid", 1], ["oval", "solid", 2], ["oval", "solid", 3])
-            True
-
-            >>> myGame.is_set(["oval", "solid", 1], ["oval", "striped", 2], ["oval", "solid", 3])
-            False
         """
 
         self.card1 = card1
         self.card2 = card2
         self.card3 = card3
 
-        # Using temp variable names for easier reference
-        shape1 = card1[0]
-        shape2 = card2[0]
-        shape3 = card3[0]
+        # True if game conditions for a valid set hold; False otherwise
+        return ((card1.shape == card2.shape == card3.shape) or \
+                (card1.shape != card2.shape and \
+                 card2.shape != card3.shape and \
+                 card1.shape != card3.shape)) and \
+               ((card1.shading == card2.shading == card3.shading) or \
+                (card1.shading != card2.shading and \
+                 card2.shading != card3.shading and \
+                 card1.shading != card3.shading)) and \
+               ((card1.count == card2.count == card3.count) or \
+                (card1.count != card2.count and \
+                 card2.count != card3.count and \
+                 card1.count != card3.count))
 
-        shading1 = card1[1]
-        shading2 = card2[1]
-        shading3 = card3[1]
 
-        count1 = card1[2]
-        count2 = card2[2]
-        count3 = card3[2]
+    def find_set(self):
+        """Finds one possible set among the table cards and prints it out. The 
+           function returns a boolean indicating whether or not a set was found.
+        """
 
-        return ((shape1 == shape2 == shape3) or \
-                (shape1 != shape2 and \
-                 shape2 != shape3 and \
-                 shape1 != shape3)) and \
-               ((shading1 == shading2 == shading3) or \
-                (shading1 != shading2 and \
-                 shading2 != shading3 and \
-                 shading1 != shading3)) and \
-               ((count1 == count2 == count3) or \
-                (count1 != count2 and \
-                 count2 != count3 and \
-                 count1 != count3))
+        for card1 in self.table:
+            for card2 in self.table:
+                for card3 in self.table:
+                    if card1 != card2 != card3:
+                        if self.is_set(card1, card2, card3):
+                            print card1, card2, card3
+                            return True
 
-    def find_set():
-        pass
+        print "Sorry, no set found."
+        return False
 
 
 
 if __name__ == "__main__":
-    import doctest
 
-    result = doctest.testmod()
-    if result.failed == 0:
-        print "ALL TESTS PASSED"
+    print "\nNew card with default values:\n"
+    myCard = SetCard()
+    print myCard
+    print "\n****************************************\n"
+
+    print "New deck from an instantiation of SetGame:\n"
+    myGame = SetGame()
+    print myGame
+    print "\n****************************************\n"
+
+    print "List of 7 cards that were dealt from the deck onto the table:\n"
+    for card in myGame.deal(7):
+        print card
+    print "\n****************************************\n"
+
+    print "Let's see if there's a set currently on the table:\n"
+    print myGame.find_set()
+    print "\n****************************************\n"
