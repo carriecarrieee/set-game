06fb4a7f6cdcbec629413ea3a19ad69af4a0acb8
Carrie Li <carrie.t.li@gmail.com>
2018-10-08 02:13:24 +0000

    Added docstring tests, separated is_set() to multiple functions, rewrote find_set()

--- set.py
+++ set.py
@@ -6,7 +6,7 @@ from random import shuffle
 
 
 class SetCard:
-    """Models a Set card, and each card has a shape, shading, and count."""
+    """ Models a Set card, and each card has a shape, shading, and count."""
 
     # Class attributes
     shapes = ["oval", "squiggle", "diamond"]
@@ -14,7 +14,7 @@ class SetCard:
     counts = [1, 2, 3]
 
 
-    def __init__(self, shape="oval", shading="solid", count=1): # default card
+    def __init__(self, shape, shading, count):
         
         self.shape = shape
         self.shading = shading
@@ -22,14 +22,22 @@ class SetCard:
 
 
     def __repr__(self):
-        """Prints representation of a SetCard"""
+        """ Prints representation of a SetCard.
+
+            Test:
+
+            >>> myCard = SetCard('oval', 'striped', 3)
+            >>> print myCard
+            oval, striped, 3
+
+            """
 
         return "{}, {}, {}".format(self.shape, self.shading, self.count)
 
         
 class SetGame:
-    """Generates deck of 27 Set cards that is stored within the class instance
-       on initialization.
+    """ Generates deck of 27 Set cards that is stored within the class instance
+        on initialization. (HAS-A relationship)
     """
     
     def __init__(self):
@@ -43,7 +51,16 @@ class SetGame:
 
 
     def __repr__(self):
-        """Prints string representation of deck."""
+        """ Prints string representation of deck.
+
+            Test:
+
+            >>> myGame = SetGame()
+            >>> print myGame.deck
+            [oval, solid, 1, oval, solid, 2, oval, solid, 3, oval, striped, 1, oval, striped, 2, oval, striped, 3, oval, open, 1, oval, open, 2, oval, open, 3, squiggle, solid, 1, squiggle, solid, 2, squiggle, solid, 3, squiggle, striped, 1, squiggle, striped, 2, squiggle, striped, 3, squiggle, open, 1, squiggle, open, 2, squiggle, open, 3, diamond, solid, 1, diamond, solid, 2, diamond, solid, 3, diamond, striped, 1, diamond, striped, 2, diamond, striped, 3, diamond, open, 1, diamond, open, 2, diamond, open, 3]
+
+
+        """
 
         deck_lst = []
 
@@ -54,13 +71,24 @@ class SetGame:
 
 
     def deal(self, num_cards):
-        """In a game of Set, cards are dealt onto the table at a given time. 
-           The instance variable "table" tracks the cards that are currently on
-           the table. This function deals num_cards random cards onto the table 
-           from the deck.
-        """
+        """ In a game of Set, cards are dealt onto the table at a given time. 
+            The instance variable "table" tracks the cards that are currently on
+            the table. This function deals num_cards random cards onto the table 
+            from the deck.
 
-        self.num_cards = num_cards # Number of cards to be dealt
+            Test:
+            
+            >>> myGame = SetGame()
+            >>> len(myGame.deal(4))
+            4
+
+            >>> len(myGame.deal(5))
+            9
+
+            >>> len(myGame.table)
+            9
+
+        """
 
         shuffle(self.deck) # Shuffle cards before dealing
 
@@ -75,70 +103,99 @@ class SetGame:
 
 
     def is_set(self, card1, card2, card3):
-        """Accepts three cards and returns a boolean indicating whether the 
-           three cards make up a valid set. A set is valid if all three
-           conditions hold:
+        """ Accepts three cards and returns a boolean indicating whether the 
+            three cards make up a valid set. A set is valid if all three
+            conditions hold:
             1. Shape: all the same shape OR all different shapes
             2. Shading: all the same shading OR all different shading
             3. Count: all the same count OR all different count
+
+            True if game conditions for a valid set hold; False otherwise.
+
+            Test:
+
+            >>> card1 = SetCard('oval', 'striped', 1)
+            >>> card2 = SetCard('diamond', 'striped', 2)
+            >>> card3 = SetCard('squiggle', 'striped', 3)
+            >>> myGame = SetGame()
+            >>> myGame.is_shape_valid(card1, card2, card2)
+            False
+            >>> myGame.is_shading_valid(card1, card2, card3)
+            True
+            >>> myGame.is_count_valid(card1, card1, card2)
+            False
+            >>> myGame.is_set(card1, card2, card3)
+            True
+            >>> myGame.is_set(card1, card2, card2)
+            False
         """
 
-        self.card1 = card1
-        self.card2 = card2
-        self.card3 = card3
+        return self.is_shape_valid(card1, card2, card3) and \
+               self.is_shading_valid(card1, card2, card3) and \
+               self.is_count_valid(card1, card2, card3)
 
-        # True if game conditions for a valid set hold; False otherwise
-        return ((card1.shape == card2.shape == card3.shape) or \
+
+    def is_shape_valid(self, card1, card2, card3):
+        """ Returns True if the shape attribute is valid, False otherwise."""
+
+        return (card1.shape == card2.shape == card3.shape) or \
                 (card1.shape != card2.shape and \
                  card2.shape != card3.shape and \
-                 card1.shape != card3.shape)) and \
-               ((card1.shading == card2.shading == card3.shading) or \
+                 card1.shape != card3.shape)
+
+
+    def is_shading_valid(self, card1, card2, card3):
+        """ Returns True if the shading attribute is valid, False otherwise."""
+
+        return (card1.shading == card2.shading == card3.shading) or \
                 (card1.shading != card2.shading and \
                  card2.shading != card3.shading and \
-                 card1.shading != card3.shading)) and \
-               ((card1.count == card2.count == card3.count) or \
+                 card1.shading != card3.shading)
+
+
+    def is_count_valid(self, card1, card2, card3):
+        """ Returns True if the count attribute is valid, False otherwise."""
+
+        return (card1.count == card2.count == card3.count) or \
                 (card1.count != card2.count and \
                  card2.count != card3.count and \
-                 card1.count != card3.count))
+                 card1.count != card3.count)
 
 
     def find_set(self):
-        """Finds one possible set among the table cards and prints it out. The 
-           function returns a boolean indicating whether or not a set was found.
+        """ Finds one possible set among the table cards and prints it out.
+            The function returns a boolean indicating whether or not a set was found.
         """
 
-        for card1 in self.table:
-            for card2 in self.table:
-                for card3 in self.table:
-                    if card1 != card2 != card3:
-                        if self.is_set(card1, card2, card3):
-                            print card1, card2, card3
-                            return True
+        if len(self.table) >= 3:
+            for i in range(len(self.table)):
+                for j in range(len(self.table[1:])):                
+                    for k in range(len(self.table[2:])):
+                        if self.table[i] != self.table[j] and \
+                           self.table[j] != self.table[k] and \
+                           self.table[i] != self.table[k] and \
+                           self.is_set(self.table[i], self.table[j], self.table[k]):
+                                print self.table[i], self.table[j], self.table[k]
+                                return True
 
         print "Sorry, no set found."
         return False
 
-
+# loop through indices i.e. for i in range(0, len(self.table))
 
 if __name__ == "__main__":
 
-    print "\nNew card with default values:\n"
-    myCard = SetCard()
-    print myCard
-    print "\n****************************************\n"
+    import doctest
 
-    print "New deck from an instantiation of SetGame:\n"
     myGame = SetGame()
-    print myGame
-    print "\n****************************************\n"
-
-    print "List of cards that were dealt from the deck onto the table:\n"
     myGame.deal(4)
     myGame.deal(5)
     for card in myGame.table:
         print card
-    print "\n****************************************\n"
 
-    print "Let's see if there's a set currently on the table:\n"
+    print "\nSet currently on the table?\n"
     print myGame.find_set()
-    print "\n****************************************\n"
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print "\nALL TESTS PASSED"
